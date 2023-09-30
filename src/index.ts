6f1c90e2adb5be8b4f2f3fc3027c7828690709f9
Pal Kerecsenyi <pal@p.kerecs.com>
2023-09-30 11:27:25 +0100

    Custom firebase getters

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { type Auth, connectAuthEmulator, getAuth } from "firebase/auth"
-import { type Firestore, connectFirestoreEmulator, getFirestore } from "firebase/firestore"
-import { type Functions, connectFunctionsEmulator, getFunctions } from "firebase/functions"
+import { type Auth, connectAuthEmulator } from "firebase/auth"
+import { type Firestore, connectFirestoreEmulator } from "firebase/firestore"
+import { type Functions, connectFunctionsEmulator } from "firebase/functions"
 
 export interface DebuggableFirebaseConfig {
-    useDebugger: boolean
+    useDebugger: boolean,
     firestore: {
         host: string,
         port: number,
@@ -19,20 +19,28 @@ export interface DebuggableFirebaseConfig {
 
 export type DebuggableFirebaseConstructorOptions = Partial<DebuggableFirebaseConfig>
 
+export interface FirebaseGetters {
+    firestore(): Firestore
+    auth(): Auth
+    functions(): Functions
+}
+
 export default class DebuggableFirebase {
     private readonly options: DebuggableFirebaseConfig
+    private readonly getters: FirebaseGetters
 
-    constructor(options: DebuggableFirebaseConstructorOptions) {
+    constructor(getters: FirebaseGetters, options?: DebuggableFirebaseConstructorOptions) {
+        this.getters = getters
         this.options = {
-            useDebugger: options.useDebugger ?? process.env.NODE_ENV === "development",
-            firestore: options.firestore ?? {
+            useDebugger: options?.useDebugger ?? process.env.NODE_ENV === "development",
+            firestore: options?.firestore ?? {
                 host: "127.0.0.1",
                 port: 8080,
             },
-            auth: options.auth ?? {
+            auth: options?.auth ?? {
                 url: "http://127.0.0.1:9099",
             },
-            functions: options.functions ?? {
+            functions: options?.functions ?? {
                 host: "127.0.0.1",
                 port: 5001,
             },
@@ -43,27 +51,27 @@ export default class DebuggableFirebase {
         return this.options.useDebugger
     }
 
-    private firestore: Firestore
-    private auth: Auth
-    private functions: Functions
+    private firestore?: Firestore
+    private auth?: Auth
+    private functions?: Functions
 
     getConfiguredFirestore() {
         if (this.firestore) return this.firestore
-        this.firestore = getFirestore()
+        this.firestore = this.getters.firestore()
         if (this.useDebugger) connectFirestoreEmulator(this.firestore, this.options.firestore.host, this.options.firestore.port)
         return this.firestore
     }
 
     getConfiguredAuth() {
         if (this.auth) return this.auth
-        this.auth = getAuth()
+        this.auth = this.getters.auth()
         if (this.useDebugger) connectAuthEmulator(this.auth, this.options.auth.url)
         return this.auth
     }
 
     getConfiguredFunctions() {
         if (this.functions) return this.functions
-        this.functions = getFunctions(undefined, "europe-west2")
+        this.functions = this.getters.functions()
         if (this.useDebugger) connectFunctionsEmulator(this.functions, this.options.functions.host, this.options.functions.port)
         return this.functions
     }
